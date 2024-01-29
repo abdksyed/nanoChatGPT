e12e6d1efd8e39a4c7dbbf90fb06894856b05338
abdksyed <abdksyed@gmail.com>
2024-01-29 10:36:52 -0600

    fix y.to() if list and acc logging

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -12,6 +12,7 @@ def train_epoch(config, model, device, data_loader, epoch, optimizer, scaler):
     train_iterator = tqdm(data_loader, desc=f"Training epoch {epoch}", total=len(data_loader))
     total_loss = 0
     batch_loss = 0
+    total_acc = 0
     accumulation_counter = 0
     
     for batch in train_iterator:
@@ -19,10 +20,16 @@ def train_epoch(config, model, device, data_loader, epoch, optimizer, scaler):
 
         # Forward Pass
         with torch.cuda.amp.autocast():
-            logits, loss = model(x.to(device), y.to(device))
+            if isinstance(y, list):
+                for i in range(len(y)):
+                    y[i] = y[i].to(device)
+            else:
+                y = y.to(device)
+            acc, loss = model(x.to(device), y)
         train_iterator.set_postfix(loss=f"{loss.item():.3f}")
         total_loss += loss.item()
         batch_loss += loss.item()
+        total_acc += acc.item()
         
         # Tensorboard Logging
         # wandb.log({"loss/train": loss.item()})
@@ -44,6 +51,7 @@ def train_epoch(config, model, device, data_loader, epoch, optimizer, scaler):
             batch_loss = 0
 
     total_loss /= len(data_loader)
+    total_acc /= len(data_loader)
     
     # Save the model
     # optimizer.zero_grad(set_to_none=True)
@@ -60,26 +68,34 @@ def train_epoch(config, model, device, data_loader, epoch, optimizer, scaler):
     )
     wandb.save(str(Path(config.weights_folder) / file_name))
 
-    return total_loss
+    return total_loss, total_acc
     
 
 def val_epoch(model, device, val_loader, epoch):
     model.eval()
     val_iterator = tqdm(val_loader, desc=f"Validating epoch {epoch}", total=len(val_loader))
     total_loss = 0
+    total_acc = 0
     with torch.inference_mode():
         for batch in val_iterator:
             x,y = batch
 
             # Forward Pass
             with torch.cuda.amp.autocast():
-                logits, loss = model(x.to(device), y.to(device))
+                if isinstance(y, list):
+                    for i in range(len(y)):
+                        y[i] = y[i].to(device)
+                else:
+                    y = y.to(device)
+                acc, loss = model(x.to(device), y)
             val_iterator.set_postfix(loss=f"{loss.item():.3f}")
             total_loss += loss.item()
+            total_acc += acc.item()
 
     total_loss /= len(val_loader)
+    total_acc /= len(val_loader)
     
-    return total_loss
+    return total_loss, total_acc
 
 def train(config, model, train_dl, val_dl):
     
@@ -90,6 +106,6 @@ def train(config, model, train_dl, val_dl):
     # Scaler
     scaler = torch.cuda.amp.GradScaler()
     for epoch in range(1, config.epochs+1):
-        train_loss = train_epoch(config, model, config.device, train_dl, epoch, optimizer, scaler)
-        val_loss = val_epoch(model, config.device, val_dl, epoch)
-        wandb.log({"loss/train_epoch": train_loss, "loss/val_epoch": val_loss, "epoch":epoch})
+        train_loss, train_acc = train_epoch(config, model, config.device, train_dl, epoch, optimizer, scaler)
+        val_loss, val_acc = val_epoch(model, config.device, val_dl, epoch)
+        wandb.log({"loss/train_epoch": train_loss, "loss/val_epoch": val_loss, "acc/train_epoch": train_acc, "acc/val_epoch": val_acc, "epoch":epoch})
